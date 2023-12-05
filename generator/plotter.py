eced91811fec0d10bbbdea65d61c109ccdbcf9a2
barbadosian <jiajunm@uchicago.edu>
2023-12-04 18:17:50 -0600

    Wrapping up

--- generator/plotter.py
+++ generator/plotter.py
@@ -17,7 +17,7 @@ color_palette = [
 ]
 
 # The input data_frame must have column 'relative' and 'url', where relative in the request's integer second from the start
-def plot_frequency(ax, data_frame: pd.DataFrame, plot_url: str, mission_seconds: int, color: str, rolling_window_seconds: int = 240):
+def plot_frequency(ax, data_frame: pd.DataFrame, plot_url: str, mission_seconds: int, color: str, rolling_window_seconds: int = 240, xclip: Tuple[int, int]=(-1,-1)):
     # Filter by the URL that we want to plot
     plot_pdf = data_frame[data_frame['url'] == plot_url]
     # Get how many requests to this URL in a relative time window
@@ -34,9 +34,12 @@ def plot_frequency(ax, data_frame: pd.DataFrame, plot_url: str, mission_seconds:
     ax.plot(plot_pdf['relative'], plot_pdf['count_rolling'], label="Freq (4m rolling) [{}]".format(plot_url), color=color)
     ax.legend(loc='upper right')
     
+    if xclip != (-1,-1):
+        plot_pdf = plot_pdf.iloc[xclip[0]:xclip[1]]
+    
     return plot_pdf
 
-def plot_frequencies(data_frame: pd.DataFrame, plot_urls: List[str], mission_seconds: int, rolling_window_seconds: int = 240, xlim: Tuple[int] = (-1,-1), ylim: Tuple[int] = (0, 10), color_palette: List[str] = color_palette):
+def plot_frequencies(data_frame: pd.DataFrame, plot_urls: List[str], mission_seconds: int, rolling_window_seconds: int = 240, xlim: Tuple[int] = (-1,-1), ylim: Tuple[int] = (0, 10), xclip: Tuple[int, int]=(-1,-1), color_palette: List[str] = color_palette):
     fig, ax = plt.subplots(1, 1)
     fig.set_size_inches(18, 8)
     fig.set_dpi(600)
@@ -50,13 +53,15 @@ def plot_frequencies(data_frame: pd.DataFrame, plot_urls: List[str], mission_sec
             plot_url, 
             mission_seconds, 
             color_palette[index % len(color_palette)],
-            rolling_window_seconds
+            rolling_window_seconds,
+            xclip
         )
         
         freq_pdfs.append(freq)
         
         index += 1
-        
+    
+    
     plt.xlabel("Mission Time (sec)")
     plt.ylabel("Frequency")
     plt.ylim(ylim)
